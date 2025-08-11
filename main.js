c2abebbf8509b2468abb2f26d72a2eeb31e720c3
Noah <nbelman1@gmail.com>
2025-08-10 20:14:12 -0400

    Complete tasks 1-3

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-code
+// write a function named reverseString that takes a string as input
+// base case = return the string if it contains only one character
+// recursive case = 
+// concatenate the last char of the string with the result of the recursive call
+// on the remaining part of the string
+
+function reverseString(str) {
+    if (str.length === 1) { // base case 
+        return str;
+    } else if (str.length === 0) {
+        return str;
+    }
+    return str[str.length - 1] + reverseString(str.slice(0, str.length - 1));
+}
+
+console.log(reverseString("hello")); // Outputs: "olleh"
+console.log(reverseString("recursion"));
+console.log(reverseString("a")); // Outputs: "a"
+console.log(reverseString("")); // Outputs: ""
